e8e1936acead5e37d2fa5fcc5f07869e7483ce21
Veronika Vokounová <ver.cik@seznam.cz>
2023-01-10 20:47:44 +0100

    aktualizace

--- src/components/Main/Filter.jsx
+++ src/components/Main/Filter.jsx
@@ -5,7 +5,6 @@ import data from '../../data'
 
 const Filter = () => {
 
-
   const handlePeopleClick = () => {
 
   const filterPeople = data.filter(people => {
@@ -20,6 +19,12 @@ const Filter = () => {
   })
 }
 
+const handleCategoryClick = (category) => {
+  return (
+    console.log("kliklo se na: ", category)
+  )
+}
+
 
   return (
     <>
@@ -30,7 +35,7 @@ const Filter = () => {
         
         <div className='py-5 flex flex-col space-y-6'>
          
-         <button className='flex' onClick={handlePeopleClick}>
+         <button className='flex' onClick={handleCategoryClick("People")}>
             <Form.Check
               category="people"
               type="checkbox"
@@ -39,58 +44,58 @@ const Filter = () => {
    
           </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Premium")}>
             <Form.Check
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Premium</p>
-          </span>
+          </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Pets")}>
             <Form.Check
               inline
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Pets</p>
-          </span>
+          </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Food")}>
             <Form.Check
               inline
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Food</p>
-          </span>
+          </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Landmarks")}>
             <Form.Check
               inline
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Landmarks</p>
-          </span>
+          </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Cities")}>
             <Form.Check
               inline
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Cities</p>
-          </span>
+          </button>
 
-          <span className='flex'>
+          <button className='flex' onClick={() => handleCategoryClick("Nature")}>
             <Form.Check
               inline
               name="group1"
               type="checkbox"
             />
             <p className='ml-4'>Nature</p>
-          </span>
+          </button>
 
          </div>
 
